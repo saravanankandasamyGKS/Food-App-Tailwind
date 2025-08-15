6cc1e858baf1cb6a0aff2edef30a17eb66833712
saravanankandasamyGKS <gksaravanan008@gmail.com>
2025-08-15 07:20:53 +0530

    shimmer change

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Shimmer = () => {
   return (
     <div className="shimmer-container max-w-7xl mx-auto p-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 animate-pulse">
-      {[...Array(32)].map((_, index) => (
+      {[...Array(8)].map((_, index) => (
         <div
           key={index}
           className="shimmer-card bg-gray-300 rounded-lg h-48"
