4fead03f669aff3774818588e8d71ea9c2460a7e
saravanankandasamyGKS <gksaravanan008@gmail.com>
2025-10-01 16:33:14 +0530

    dependency array

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -6,7 +6,7 @@ const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState(null);
   useEffect(() => {
     fetchMenu();
-  });
+  }, []);
   const fetchMenu = async () => {
     const data = await fetch(MENU_API + resId);
     const json = await data.json();
